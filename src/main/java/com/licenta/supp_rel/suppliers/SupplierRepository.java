b591e9ed1472af9cdbe18b1672c8a70b4811e639
stefa <stefanp1999@yahoo.com>
2023-03-21 12:53:17 +0200

    added ToleranceController methods for add update delete

--- src/main/java/com/licenta/supp_rel/suppliers/SupplierRepository.java
+++ src/main/java/com/licenta/supp_rel/suppliers/SupplierRepository.java
@@ -2,5 +2,8 @@ package com.licenta.supp_rel.suppliers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SupplierRepository extends JpaRepository<Supplier, String> {
+    Optional<Supplier> findById(String id);
 }
--- src/main/java/com/licenta/supp_rel/tolerances/ToleranceController.java
+++ src/main/java/com/licenta/supp_rel/tolerances/ToleranceController.java
@@ -1,10 +1,9 @@
 package com.licenta.supp_rel.tolerances;
 
+import com.licenta.supp_rel.suppliers.SupplierRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,17 +12,72 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ToleranceController {
     private final ToleranceService toleranceService;
+    @Autowired
+    ToleranceRepository toleranceRepository;
+    @Autowired
+    SupplierRepository supplierRepository;
     @GetMapping("get-by-plantId-supplierId-materialCode")
     public List<Tolerance> getTolerancesByPlantIdSupplierIdMaterialCode(@RequestParam(value = "plantId", required = false) String plantId,
                                                                         @RequestParam(value = "supplierId", required = false) String supplierId,
                                                                         @RequestParam(value = "materialCode", required = false) String materialCode){
-        if(plantId == null || plantId.equals(""))
+        if(plantId == null || plantId.equals("") || plantId.equals("null") || plantId.equals("All"))
             plantId = "*";
-        if(supplierId == null || supplierId.equals(""))
+        if(supplierId == null || supplierId.equals("") || supplierId.equals("null") || supplierId.equals("All"))
             supplierId = "*";
-        if(materialCode == null || materialCode.equals(""))
+        if(materialCode == null || materialCode.equals("") || materialCode.equals("null") || materialCode.equals("All"))
             materialCode = "*";
         return toleranceService.findTolerancesByPlantIdSupplierIdMaterialCode(plantId, supplierId, materialCode);
+    }
+
+    @PostMapping("add")
+    public Tolerance addTolerance(@RequestParam("plantId") String plantId,
+                                  @RequestParam("supplierId") String supplierId,
+                                  @RequestParam("materialCode") String materialCode,
+                                  @RequestParam("qtyUpperLimit") Float qtyUpperLimit,
+                                  @RequestParam("qtyLowerLimit") Float qtyLowerLimit,
+                                  @RequestParam("dayUpperLimit") Integer dayUpperLimit,
+                                  @RequestParam("dayLowerLimit") Integer dayLowerLimit){
+        Tolerance tolerance = new Tolerance();
+        tolerance.setPlantId(plantId);
+        tolerance.setSupplier(supplierRepository.findById(supplierId).orElse(null));
+        tolerance.setMaterialCode(materialCode);
+        tolerance.setQtyUpperLimit(qtyUpperLimit);
+        tolerance.setQtyLowerLimit(qtyLowerLimit);
+        tolerance.setDayUpperLimit(dayUpperLimit);
+        tolerance.setDayLowerLimit(dayLowerLimit);
+        toleranceRepository.save(tolerance);
+        return tolerance;
+    }
+
+    @PutMapping("update")
+    public Tolerance updateTolerance(@RequestParam("id") Integer id,
+                                     @RequestParam("plantId") String plantId,
+                                     @RequestParam("supplierId") String supplierId,
+                                     @RequestParam("materialCode") String materialCode,
+                                     @RequestParam("qtyUpperLimit") Float qtyUpperLimit,
+                                     @RequestParam("qtyLowerLimit") Float qtyLowerLimit,
+                                     @RequestParam("dayUpperLimit") Integer dayUpperLimit,
+                                     @RequestParam("dayLowerLimit") Integer dayLowerLimit){
+        Tolerance tolerance = toleranceRepository.findById(id).orElse(null);
+        if(tolerance!=null) {
+            tolerance.setPlantId(plantId);
+            tolerance.setSupplier(supplierRepository.findById(supplierId).orElse(null));
+            tolerance.setMaterialCode(materialCode);
+            tolerance.setQtyUpperLimit(qtyUpperLimit);
+            tolerance.setQtyLowerLimit(qtyLowerLimit);
+            tolerance.setDayUpperLimit(dayUpperLimit);
+            tolerance.setDayLowerLimit(dayLowerLimit);
+            toleranceRepository.save(tolerance);
+        }
+        return tolerance;
+    }
+
+    @DeleteMapping("delete")
+    public Tolerance deleteTolerance(@RequestParam("id") Integer id){
+        Tolerance tolerance = toleranceRepository.findById(id).orElse(null);
+        if(tolerance!=null)
+            toleranceRepository.delete(tolerance);
+        return tolerance;
     }
 
 }
